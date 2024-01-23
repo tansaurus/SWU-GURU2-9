dccc64b3df60f631ae2bd1fb6c243439a02cef60
ju2h <108256245+ju2h@users.noreply.github.com>
2024-01-24 00:30:43 +0900

    비밀번호 찾기 화면

---
+++ app/src/main/java/om/androidbook/medicine4/FindPasswordActivity.kt
@@ -0,0 +1,11 @@
+package om.androidbook.medicine4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_password)
+    }
+}
