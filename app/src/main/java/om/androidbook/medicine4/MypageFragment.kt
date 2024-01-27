1676a0f34fa0f8d6ab6d97a6a9bd6b02b2b04dbb
ju2h <108256245+ju2h@users.noreply.github.com>
2024-01-27 22:45:24 +0900

    마이페이지 액티비티로 수정 후 기능 연결

--- app/src/main/java/om/androidbook/medicine4/MypageFragment.kt
+++ app/src/main/java/om/androidbook/medicine4/MypageFragment.kt
@@ -1,28 +1,32 @@
 package om.androidbook.medicine4
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.widget.AppCompatButton
 
 class MypageFragment : Fragment() {
-    @SuppressLint("MissingInflatedId")
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_mypage, container, false)
-        val mActivity = activity as NaviActivity
 
-        // 프로필 수정 버튼 클릭
-        val editProfileButton = rootView.findViewById(R.id.editProfileButton) as AppCompatButton
-        editProfileButton.setOnClickListener {
-            mActivity.changeMyPageFragment("TAG_PROFILE_EDIT", MypageProfileEditFragment())
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // 이 프래그먼트에 대한 레이아웃을 인플레이트
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 프로필 수정 버튼
+        val profileEditButton = view.findViewById<Button>(R.id.editProfileButton)
+
+        // 프로필 수정 버튼 클릭 시 이벤트
+        profileEditButton.setOnClickListener {
+            // 프로필 수정 화면으로 이동
+            val intent = Intent(requireContext(), ProfileEditActivity::class.java)
+            startActivity(intent)
         }
-
-        return rootView
     }
 }
