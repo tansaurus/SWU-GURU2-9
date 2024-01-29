ea8eb0d8d7dc5f72fbab4a1c0a6e090bceea374f
starday119 <133083872+starday119@users.noreply.github.com>
2024-01-29 14:27:40 +0900

    필요없는 파일 삭제

--- app/src/main/java/om/androidbook/medicine4/ListLayout.kt
+++
@@ -1,7 +0,0 @@
-package om.androidbook.medicine4
-
-class ListLayout(val name: String,      // 장소명
-                 val road: String,      // 도로명 주소
-                 val address: String,   // 지번 주소
-                 val x: Double,         // 경도(Longitude)
-                 val y: Double)         // 위도(Latitude)
